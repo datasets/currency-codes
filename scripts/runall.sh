0c0cad30a5426b73975802bc6ba69d669a514303
gradedSystem <homesiknessgerald@gmail.com>
2024-10-20 12:57:30 +0700

    [update][s] Updating shell script

--- scripts/runall.sh
+++ scripts/runall.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# extract currency-code data
+# Force UTF-8 encoding
+export LC_ALL=en_US.UTF-8
+LANG=C.UTF-8
 
-# check for required functions
-command -v recode >/dev/null 2>&1 || { echo "Script requires 'recode' package. Aborting"; exit 1; }
-command -v sed >/dev/null 2>&1 || { echo "Script requires 'sed' package. Aborting"; exit 1; }
+# extract currency-code data
 
 # where to extract data to
 outdir=../data
@@ -17,8 +17,8 @@ if [ ! -d ./cache ]; then
   mkdir cache
 fi;
 echo Downloading XML files...
-curl -s -o cache/a1.xml "https://www.currency-iso.org/dam/downloads/lists/list_one.xml"
-curl -s -o cache/a2.xml "https://www.currency-iso.org/dam/downloads/lists/list_three.xml"
+curl -s -o cache/a1.xml "https://www.six-group.com/dam/download/financial-information/data-center/iso-currrency/lists/list-one.xml"
+curl -s -o cache/a2.xml "https://www.six-group.com/dam/download/financial-information/data-center/iso-currrency/lists/list-three.xml"
 
 # write headers
 echo "Entity,Currency,AlphabeticCode,NumericCode,MinorUnit,WithdrawalDate" > ${outfile}
@@ -40,12 +40,12 @@ do
   fi
 
   # get all rows
-  xmllint --xpath "${tblroot}/${rowname}" $f > cache/tmp
+  xmllint --xpath "${tblroot}/${rowname}" --encode UTF-8 $f > cache/tmp
   # remove line endings (one big line)
   sed -i.tmp -e ':a' -e 'N' -e '$!ba' -e 's/\n/ /g' cache/tmp
   # remove tabs
   sed -i.tmp 's/\t//g' cache/tmp
-  # separte each row to new line
+  # separate each row to new line
   sed -i.tmp  "s/<\/${rowname}>/\n/g" cache/tmp
 
   # loop of rows
@@ -53,19 +53,16 @@ do
   c=1
   while read LINE
   do
-    # break up row into lines
-    #LSPLIT=`echo "$LINE" | sed "s/>/>\n/g" | sed "s/<\//\n<\//g" | sed "s/^ //g"`
     LSPLIT=`echo "$LINE" | sed -e "s/>/>\n/g" -e "s/<\//\n<\//g" | sed "s/^ //g"`
 
     # look for each column
     a=""  # csv line
     for (( j = 0 ; j < ${#nodes[@]} ; j++ ))
     do
-      # search for this tag
       nn=${nodes[$j]}
-      valq=`echo "$LSPLIT" | sed -nr "/<${nn}(\sIsFund=.*)?>/,/<\/${nn}>/p" | head -n-1 | tail -n+2`
-      val=`echo "$valq" | sed "s/\"/\"\"/g"`  # replace single double quote with double double quote
-      # check for a quote or comma in val, if non-empty
+      valq=`echo "$LSPLIT" | sed -nr "/<${nn}(\sIsFund=.*)?>/,/<\/${nn}>/p" | sed '$d' | tail -n +2`
+      val=`echo "$valq" | sed "s/\"/\"\"/g"`
+
       if [[ ! -z $val ]]; then
         if [[ $val == *,* || $val != $valq ]]; then
           val="\"${val}\""  # val needs to be quoted
@@ -76,20 +73,18 @@ do
         val="-"
       fi
 
-      # concat to csv row
       if [[ -z $a ]]; then
-        # first column
         a="$val"
       else
-        # non-first column
         a="${a},${val}"
       fi
     done
 
-    # append line to file
-    echo "$a" >> ${outfile}
+    # Check if the row is non-empty before writing to the CSV
+    if [[ -n "$a" ]]; then
+      echo "$a" >> ${outfile}
+    fi
 
-    # count files, print status
     c=$(($c + 1))
     if [ $(($c % 10)) = 0 ]; then
       echo -n "."
@@ -98,15 +93,9 @@ do
   echo " "
 done
 
-# convert HTML special entities to UTF-8
-recode html..utf8 ${outfile} # convert in place
-
-# move raw data to archive
-if [[ ! -d ../archive ]]; then
-  mkdir ../archive
-fi
-mv cache/a1.xml ../archive/table_a1.xml
-mv cache/a2.xml ../archive/table_a2.xml
+# convert special entities and ensure UTF-8 output
+iconv -f ISO-8859-1 -t UTF-8 ${outfile} -o ${outfile}.utf8
+mv ${outfile}.utf8 ${outfile}
 
 # clean up
 rm -r cache/
